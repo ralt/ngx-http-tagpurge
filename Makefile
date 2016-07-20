4872062532075de8f290f55d21fd219c2eb5d17b
Florian Margaine <florian@margaine.com>
2016-07-21 01:41:03 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+NGINX_VERSION=1.11.2
+NGINX_FOLDER=vendor/nginx-$(NGINX_VERSION)
+SOURCES:=$(wildcard src/*.c) $(wildcard src/*.h) config
+
+all: build/nginx/sbin/nginx
+
+build/nginx/sbin/nginx: $(NGINX_FOLDER)/.configured $(SOURCES) build/nginx
+	cd $(NGINX_FOLDER) && \
+	make -j16 && \
+	make install
+
+	ln -sfn $(PWD)/nginx.conf build/nginx/conf/nginx.conf
+
+build/nginx:
+	mkdir -p build/nginx/conf build/nginx/logs
+
+$(NGINX_FOLDER)/.configured: $(NGINX_FOLDER)
+	cd $(NGINX_FOLDER) && \
+	CFLAGS="" ./configure \
+		--with-debug \
+		--prefix=$(PWD)/build/nginx \
+		--conf-path=conf/nginx.conf \
+		--error-log-path=logs/error.log \
+		--http-log-path=logs/access.log \
+		--add-dynamic-module=../..
+	touch $@
+
+$(NGINX_FOLDER): vendor
+	cd vendor && \
+	curl -O http://nginx.org/download/nginx-$(NGINX_VERSION).tar.gz && \
+	tar xf nginx-$(NGINX_VERSION).tar.gz && \
+	rm -f nginx-$(NGINX_VERSION).tar.gz
+
+vendor:
+	mkdir -p vendor
---
+++ src/ngx_tagpurge_module.c
@@ -0,0 +1,70 @@
+#include <nginx.h>
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+ngx_module_t ngx_tagpurge_module;
+
+static ngx_int_t
+ngx_tagpurge_handler(ngx_http_request_t *r)
+{
+	if (r->main->internal)
+	{
+		return NGX_DECLINED;
+	}
+
+	r->main->internal = 1;
+
+	ngx_table_elt_t *h;
+	h = ngx_list_push(&r->headers_out.headers);
+	h->hash = 1;
+	ngx_str_set(&h->key, "X-NGINX-Tutorial");
+	ngx_str_set(&h->value, "Hello World!");
+
+	return NGX_DECLINED;
+}
+
+static ngx_int_t
+ngx_tagpurge_init(ngx_conf_t *cf)
+{
+	ngx_http_handler_pt *h;
+	ngx_http_core_main_conf_t *cmcf;
+
+	cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+	if (h == NULL)
+	{
+		return NGX_ERROR;
+	}
+
+	*h = ngx_tagpurge_handler;
+
+	return NGX_OK;
+}
+
+static ngx_http_module_t ngx_tagpurge_module_ctx = {
+	NULL,			/* preconfiguration */
+	ngx_tagpurge_init,      /* postconfiguration */
+	NULL,                   /* create main configuration */
+	NULL,                   /* init main configuration */
+	NULL,                   /* create server configuration */
+	NULL,                   /* merge server configuration */
+	NULL,                   /* create location configuration */
+	NULL                    /* merge location configuration */
+};
+
+ngx_module_t ngx_tagpurge_module = {
+	NGX_MODULE_V1,
+	&ngx_tagpurge_module_ctx,	/* module context */
+	NULL,				/* module directives */
+	NGX_HTTP_MODULE,		/* module type */
+	NULL,				/* init master */
+	NULL,				/* init module */
+	NULL,				/* init process */
+	NULL,				/* init thread */
+	NULL,				/* exit thread */
+	NULL,				/* exit process */
+	NULL,				/* exit master */
+	NGX_MODULE_V1_PADDING
+};
